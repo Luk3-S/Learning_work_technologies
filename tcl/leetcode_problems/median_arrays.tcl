863de17b6b9f7d8c60170f21bca6427da21ebcc8
Luk3-S <lukesp63@gmail.com>
2020-09-04 16:30:02 +0100

    median arrays refactored, 5 tests passed

--- tcl/leetcode_problems/median_arrays.tcl
+++ tcl/leetcode_problems/median_arrays.tcl
@@ -1,31 +1,31 @@
 proc median_value {l1 l2} {
     set total_length [expr [llength $l1] + [llength $l2]]
     set is_odd [expr $total_length %2]
-    if {[expr $is_odd == 1]} {
+    
+    if {[expr $is_odd == 1]} { ;# if combined length of lists is odd
         set midpoint [expr $total_length /2]
         if {[expr $midpoint == [llength $l1]]} { ;# if midpoint == length of l1, then median is 0th element of 
             set median  [lindex $l2 0]
-            return $median
         } else {
-            set median [lindex $l1 $midpoint]
-            return $median
+            set median [lindex $l1 $midpoint] 
         }
-    } else {
-        set midpoint2 [expr $total_length /2]
-        set midpoint1 [expr $midpoint2 - 1]
-            if {[expr [llength $l1 ] > [llength $l2]]} {
-                set ll $l1
-                set sl $l2
-                set med2 [expr $total_length /2]
-                set med1 [expr $midpoint2 - 1]
-            } else {
-                set ll $l2
-                set sl $l1
-                set med1 [expr ($midpoint1 - [llength $sl])]
-                set med2 [expr ($midpoint2 - [llength $sl])]
-            }
-            set median  [expr ([lindex $ll $med1] + [lindex $ll $med2]) / 2.0]
-            return $median
+    } else { ;# even combined list size
+            if {[expr [llength $l1 ] > [llength $l2]]} { ;# if l1 > l2 in size and total length is equal, midpoints will be l1[end] & l1[end - 1]
+                set median [expr {([lindex $l1 end] + [lindex $l1 end-1])} / 2.0]
+            } 
+            if {[expr [llength $l1] == [llength $l2]]} { ;# if l1 == l2 in size, midpoints are l1[end] & l2[0]
+                set median [expr ([lindex $l1 end] + [lindex $l2 0]) / 2.0]
+            } 
+            if {[expr [llength $l1] < [llength $l2]]} { ;# l2 > l1 in size, midpoints are l2[0] & l2[1]
+                set median  [expr {([lindex $l2 0] + [lindex $l2 1])} / 2.0]
+            } 
         }
-    }
-puts "Median value is: [median_value {1} {2 3 4}]"
+    return $median
+}
+puts "Median value is: [median_value {1 2 3 4} {4 6}]"
+puts "Median value is: [median_value {1} {4 6}]"
+puts "Median value is: [median_value {1 2} {4}]"
+puts "Median value is: [median_value {1 2} {3 4 5 6}]"
+puts "Median value is: [median_value {1 2 3 4} {5 6 7 8}]"
+
+
